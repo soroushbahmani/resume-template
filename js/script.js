00b335eaa9678edd636c6a2cc7033923e54859b0
soroushbahmani <soroushbibi@gmail.com>
2023-10-03 19:56:27 +0330

    feat : added modal [gallery]

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
 // typing...
-var typed = new Typed(".typing",{
-    loop:true,
-    backDelay:1000,
-    strings:["Web Developer" , "Front-End Developer"],
-    typeSpeed:150,
-    backSpeed:100,
+var typed = new Typed(".typing", {
+    loop: true,
+    backDelay: 1000,
+    strings: ["Web Developer", "Front-End Developer"],
+    typeSpeed: 150,
+    backSpeed: 100,
 })
 
 // aside =>  nav => a => active
@@ -20,35 +20,52 @@ const navLink = document.querySelectorAll(".aside .nav a");
 
 // scrool
 
-  
-document.addEventListener("scroll",(e)=> {
-    if(document.scrollingElement.scrollTop >= 0 && document.scrollingElement.scrollTop < 668){
-        navLink.forEach(e=>{
+
+document.addEventListener("scroll", (e) => {
+    if (document.scrollingElement.scrollTop >= 0 && document.scrollingElement.scrollTop < 668) {
+        navLink.forEach(e => {
             e.classList.remove("active")
         })
         navLink[0].classList.add('active')
     }
-    else if(document.scrollingElement.scrollTop >= 668 && document.scrollingElement.scrollTop <4200 ){
-        navLink.forEach(e=>{
+    else if (document.scrollingElement.scrollTop >= 668 && document.scrollingElement.scrollTop < 4200) {
+        navLink.forEach(e => {
             e.classList.remove("active")
         })
         navLink[1].classList.add('active')
     }
-    else if(document.scrollingElement.scrollTop >= 4200 && document.scrollingElement.scrollTop < 5200){
-        navLink.forEach(e=>{
+    else if (document.scrollingElement.scrollTop >= 4200 && document.scrollingElement.scrollTop < 5200) {
+        navLink.forEach(e => {
             e.classList.remove("active")
         })
         navLink[2].classList.add('active')
     }
-    else if(document.scrollingElement.scrollTop >= 5200 && document.scrollingElement.scrollTop < 6000){
-        navLink.forEach(e=>{
+    else if (document.scrollingElement.scrollTop >= 5200 && document.scrollingElement.scrollTop < 6000) {
+        navLink.forEach(e => {
             e.classList.remove("active")
         })
         navLink[3].classList.add('active')
-    } else if(document.scrollingElement.scrollTop >= 6000 ){
-        navLink.forEach(e=>{
+    } else if (document.scrollingElement.scrollTop >= 6000) {
+        navLink.forEach(e => {
             e.classList.remove("active")
         })
         navLink[4].classList.add('active')
     }
 })
+
+
+
+let open = false;
+const modal = (img) => {
+    const modal_elemant = document.getElementById('modal');
+    const image_modal = document.getElementById('image_modal');
+    
+    if (img) {
+        const image_element = document.getElementById(img);
+        const image_element_value = image_element.attributes.getNamedItem('src').nodeValue;
+        image_modal.setAttribute('src',image_element_value)
+    }
+
+    modal_elemant.classList.toggle('hide')
+
+}
